95a342891246509b51d2798d3ffa4abfd1cbb25f
Rich <Rich@DESKTOP-O9UJLSI>
2021-03-22 20:17:54 +0000

    start of the gui

---
+++ Sniffer/Sniffer/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace Sniffer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.File = new System.Windows.Forms.ToolStripMenuItem();
+            this.options = new System.Windows.Forms.ToolStripMenuItem();
+            this.about = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Number = new System.Windows.Forms.ColumnHeader();
+            this.Time = new System.Windows.Forms.ColumnHeader();
+            this.Source = new System.Windows.Forms.ColumnHeader();
+            this.Source_Port = new System.Windows.Forms.ColumnHeader();
+            this.Destination = new System.Windows.Forms.ColumnHeader();
+            this.Destination_Port = new System.Windows.Forms.ColumnHeader();
+            this.Protocol = new System.Windows.Forms.ColumnHeader();
+            this.Package_Size = new System.Windows.Forms.ColumnHeader();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.File,
+            this.options,
+            this.about});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1270, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // File
+            // 
+            this.File.Name = "File";
+            this.File.Size = new System.Drawing.Size(37, 20);
+            this.File.Text = "File";
+            // 
+            // options
+            // 
+            this.options.Name = "options";
+            this.options.Size = new System.Drawing.Size(61, 20);
+            this.options.Text = "Options";
+            // 
+            // about
+            // 
+            this.about.Name = "about";
+            this.about.Size = new System.Drawing.Size(52, 20);
+            this.about.Text = "About";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(1270, 811);
+            this.splitContainer1.SplitterDistance = 440;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1263, 433);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sniffed Packets";
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Number,
+            this.Time,
+            this.Source,
+            this.Source_Port,
+            this.Destination,
+            this.Destination_Port,
+            this.Protocol,
+            this.Package_Size});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(6, 22);
+            this.listView1.MinimumSize = new System.Drawing.Size(-2, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1251, 405);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // Number
+            // 
+            this.Number.Text = "Number";
+            this.Number.Width = 150;
+            // 
+            // Time
+            // 
+            this.Time.Text = "Time";
+            this.Time.Width = 150;
+            // 
+            // Source
+            // 
+            this.Source.Text = "Source";
+            this.Source.Width = 150;
+            // 
+            // Source_Port
+            // 
+            this.Source_Port.Text = "Source Port";
+            this.Source_Port.Width = 150;
+            // 
+            // Destination
+            // 
+            this.Destination.Text = "Destination";
+            this.Destination.Width = 150;
+            // 
+            // Destination_Port
+            // 
+            this.Destination_Port.DisplayIndex = 6;
+            this.Destination_Port.Text = "Destination Port";
+            this.Destination_Port.Width = 150;
+            // 
+            // Protocol
+            // 
+            this.Protocol.DisplayIndex = 5;
+            this.Protocol.Text = "Protocol";
+            this.Protocol.Width = 150;
+            // 
+            // Package_Size
+            // 
+            this.Package_Size.Text = "Package Size";
+            this.Package_Size.Width = 150;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer2.Size = new System.Drawing.Size(1270, 367);
+            this.splitContainer2.SplitterDistance = 423;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1270, 835);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "NetSniff";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.ToolStripMenuItem File;
+        private System.Windows.Forms.ToolStripMenuItem options;
+        private System.Windows.Forms.ToolStripMenuItem about;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Number;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.ColumnHeader Source;
+        private System.Windows.Forms.ColumnHeader Source_Port;
+        private System.Windows.Forms.ColumnHeader Destination;
+        private System.Windows.Forms.ColumnHeader Destination_Port;
+        private System.Windows.Forms.ColumnHeader Protocol;
+        private System.Windows.Forms.ColumnHeader Package_Size;
+    }
+}
+
---
+++ Sniffer/Sniffer/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sniffer
+{
+    public partial class Form1 : Form
+    {
+        string itemNumber = "", time = "", source = "", sourcePort = "", destination = "", destinationPort = "", protocol = "", packageSize = "";
+        public Form1()
+        {
+            InitializeComponent();
+            addItems();
+        }
+
+        private void addItems()
+        {
+            listView1.Items.Add("Test1");
+            listView1.Items.Add("Test2");
+            listView1.Items.Add("Test3");
+            listView1.Items.Add("Test4");
+            listView1.Items.Add("Test5");
+            listView1.Items.Add("Test6");
+            listView1.Items.Add("Test7");
+            listView1.Items.Add("Test8");
+            listView1.Items.Add("Test9");
+            listView1.Items.Add("Test10");
+            listView1.Items.Add("Test11");
+            listView1.Items.Add("Test12");
+            listView1.Items.Add("Test13");
+            listView1.Items.Add("Test14");
+            listView1.Items.Add("Test15");
+            listView1.Items.Add("Test16");
+            listView1.Items.Add("Test17");
+            listView1.Items.Add("Test18");
+        }
+    
+
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
