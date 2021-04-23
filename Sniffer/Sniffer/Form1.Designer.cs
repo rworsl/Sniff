6339e850bd639e59ec60ef4259f8d36086150a59
Rich <Rich@DESKTOP-O9UJLSI>
2021-04-23 21:12:49 +0100

    setting the UI up

--- Sniffer/Sniffer/Form1.Designer.cs
+++ Sniffer/Sniffer/Form1.Designer.cs
@@ -45,6 +45,9 @@ namespace Sniffer
             this.Protocol = new System.Windows.Forms.ColumnHeader();
             this.Package_Size = new System.Windows.Forms.ColumnHeader();
             this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -95,6 +98,9 @@ namespace Sniffer
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
             this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
             // 
             // splitContainer1.Panel2
@@ -110,9 +116,9 @@ namespace Sniffer
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox1.Controls.Add(this.listView1);
-            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Location = new System.Drawing.Point(4, 35);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1263, 433);
+            this.groupBox1.Size = new System.Drawing.Size(1263, 402);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Sniffed Packets";
@@ -136,9 +142,9 @@ namespace Sniffer
             this.listView1.GridLines = true;
             this.listView1.HideSelection = false;
             this.listView1.Location = new System.Drawing.Point(6, 22);
-            this.listView1.MinimumSize = new System.Drawing.Size(-2, 0);
+            this.listView1.MinimumSize = new System.Drawing.Size(4, 4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(1251, 405);
+            this.listView1.Size = new System.Drawing.Size(1251, 374);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -198,6 +204,32 @@ namespace Sniffer
             this.splitContainer2.SplitterDistance = 423;
             this.splitContainer2.TabIndex = 0;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(317, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Sniffer.Properties.Resources.start1;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(336, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 25);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(368, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(25, 25);
+            this.button2.TabIndex = 2;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -240,6 +272,9 @@ namespace Sniffer
         private System.Windows.Forms.ColumnHeader Destination_Port;
         private System.Windows.Forms.ColumnHeader Protocol;
         private System.Windows.Forms.ColumnHeader Package_Size;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Sniffer/Sniffer/Form1.cs
+++ Sniffer/Sniffer/Form1.cs
@@ -1,78 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using PcapDotNet.Core;
+using SharpPcap;
 
 namespace Sniffer
 {
     public partial class Form1 : Form
     {
         string itemNumber = "", time = "", source = "", sourcePort = "", destination = "", destinationPort = "", protocol = "", packageSize = "";
+        IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
         public Form1()
         {
             InitializeComponent();
+            setAdapters();
             addItems();
         }
 
         private void addItems()
         {
-            listView1.Items.Add("Test1");
-            listView1.Items.Add("Test2");
-            listView1.Items.Add("Test3");
-            listView1.Items.Add("Test4");
-            listView1.Items.Add("Test5");
-            listView1.Items.Add("Test6");
-            listView1.Items.Add("Test7");
-            listView1.Items.Add("Test8");
-            listView1.Items.Add("Test9");
-            listView1.Items.Add("Test10");
-            listView1.Items.Add("Test11");
-            listView1.Items.Add("Test12");
-            listView1.Items.Add("Test13");
-            listView1.Items.Add("Test14");
-            listView1.Items.Add("Test15");
-            listView1.Items.Add("Test16");
-            listView1.Items.Add("Test17");
-            listView1.Items.Add("Test18");
+            //listView1.Items.Add("Test1");
+            //listView1.Items.Add("Test2");
+            //listView1.Items.Add("Test3");
+            //listView1.Items.Add("Test4");
+            //listView1.Items.Add("Test5");
+            //listView1.Items.Add("Test6");
+            //listView1.Items.Add("Test7");
+            //listView1.Items.Add("Test8");
+            //listView1.Items.Add("Test9");
+            //listView1.Items.Add("Test10");
+            //listView1.Items.Add("Test11");
+            //listView1.Items.Add("Test12");
+            //listView1.Items.Add("Test13");
+            //listView1.Items.Add("Test14");
+            //listView1.Items.Add("Test15");
+            //listView1.Items.Add("Test16");
+            //listView1.Items.Add("Test17");
+            //listView1.Items.Add("Test18");
             testSocket();
         }
     
         private void testSocket()
         {
-            var mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Raw,
-                                   ProtocolType.IP);
-
-            // Bind the socket to the selected IP address
-            mainSocket.Bind(newIPEndPoint(IPAddress.Parse(cmbInterfaces.Text), 0));
-
-            // Set the socket options
-            mainSocket.SetSocketOption(SocketOptionLevel.IP,  //Applies only to IP packets
-                                       SocketOptionName.HeaderIncluded, //Set the include header
-                                       true);                           //option to true
-
-            byte[] byTrue = new byte[4]{ 1, 0, 0, 0};
-            byte[] byOut = new byte[4];
-
-            //Socket.IOControl is analogous to the WSAIoctl method of Winsock 2
-            mainSocket.IOControl(IOControlCode.ReceiveAll,  //SIO_RCVALL of Winsock
-                                 byTrue, byOut);
-
-            //Start receiving the packets asynchronously
-            mainSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None,
-                                    newAsyncCallback(OnReceive), null);
 
         }
 
-        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        private void setAdapters()
         {
-
+            IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
+            for (int i = 0; i != allDevices.Count(); i++)
+            {
+                LivePacketDevice device = allDevices[i];
+                listView1.Items.Add(device.Description);
+            }
         }
     }
 }
