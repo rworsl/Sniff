db2dae1fa725c6bb9ddf0b24f3de390f1d487cc5
Rich <Rich@DESKTOP-O9UJLSI>
2021-04-26 20:48:12 +0100

    kind of stared working, needs async setup

--- Sniffer/Sniffer/Form1.cs
+++ Sniffer/Sniffer/Form1.cs
@@ -1,22 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using PacketDotNet;
 using PcapDotNet.Core;
-using SharpPcap;
+using PcapDotNet.Packets;
 
 namespace Sniffer
 {
     public partial class Form1 : Form
     {
         string itemNumber = "", time = "", source = "", sourcePort = "", destination = "", destinationPort = "", protocol = "", packageSize = "";
-
+        Boolean running = false;
         private void Button1_Click(object sender, EventArgs e)
         {
             GetAdapter();
+            StartListening();
         }
 
-        PacketDevice selectedDevice;
+        int selectedDevice;
+        IList<LivePacketDevice> allDevices = LivePacketDevice.AllLocalMachine;
 
         public Form1()
         {
@@ -26,8 +30,7 @@ namespace Sniffer
 
         private void GetAdapter()
         {
-            selectedDevice = (PacketDevice)adapterSelect.SelectedItem;
-            packetOutBox.Items.Add(selectedDevice.ToString());
+            selectedDevice = adapterSelect.SelectedIndex;
         }
 
         private void SetAdapters()
@@ -38,6 +41,45 @@ namespace Sniffer
                 LivePacketDevice device = allDevices[i];
                 adapterSelect.Items.Add(device.Description);
             }
+        }
+
+        public async void StartListening()
+        {
+            string output = "";
+            int total = 0;
+            using (PacketCommunicator communicator =
+                allDevices[selectedDevice].Open(65536,                      // portion of the packet to capture
+                                                                            // 65536 guarantees that the whole packet will be captured on all the link layers
+                                    PacketDeviceOpenAttributes.Promiscuous, // promiscuous mode
+                                    10000))                                  // read timeout
+            {
+
+                // start the capture
+
+                PcapDotNet.Packets.Packet packet;
+                do
+                {
+                    PacketCommunicatorReceiveResult result = communicator.ReceivePacket(out packet);
+                    switch (result)
+                    {
+                        case PacketCommunicatorReceiveResult.Timeout:
+                            // Timeout elapsed
+                            total += 1;
+                            continue;
+                        case PacketCommunicatorReceiveResult.Ok:
+                            AddText(packet);
+                            total += 1;
+                            break;
+                        default:
+                            throw new InvalidOperationException("The result " + result + " should never be reached here");
+                    }
+                } while (total < 100);
+            }
+        }
+
+        public void AddText(PcapDotNet.Packets.Packet packet)
+        {
+            packetOutBox.Items.Add(packet.ToString());
         }
     }
 }
